4922f999678934a6f44e64e922d4375dfacb433b
Dnyaneshwari Khatiwada <137375437+diya200d@users.noreply.github.com>
2024-07-25 19:41:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let searchbar=document.querySelector('#search-bar');
+let searchbox=document.querySelector('.search-box');
+let shop=document.querySelector('#shop-cart');
+let shopcart=document.querySelector('.shopping-cart');
+let menubar=document.querySelector('#menu-bar');
+let mynav=document.querySelector('.navbar');
+
+
+
+
+searchbar.onclick=()=>{
+    searchbox.classList.toggle('active');
+}
+
+shop.onclick=()=>{
+    shopcart.classList.toggle('active');
+}
+menubar.onclick=()=>{
+    mynavr.classList.toggle('active');
+}
+
+
+
